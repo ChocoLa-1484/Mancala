b77fbb29399a8b7a539212dcc1e8eb6b58c01483
ChocoLa-1484 <naiyanyang@gmail.com>
2023-04-06 14:22:34 +0800

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by 楊乃諺 on 2023/4/6.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/MancalaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MancalaApp.swift
+//  Shared
+//
+//  Created by 楊乃諺 on 2023/4/6.
+//
+
+import SwiftUI
+
+@main
+struct MancalaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
